d146a2f7919eaeebfd37076d595dc0618c1d092d
sasi1992 <mohammed.sase28@gmail.com>
2017-05-13 15:30:13 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#appdemo2").innerHTML="<button type='button' class='btn btn-primary' id='showname'> Click me </button";
+$('#showname').click(function(){
+	alert("I am mohamed :)");
+});
